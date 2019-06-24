67b239e875dcd2370e583411dc05f7a016c6e811
jasontherion <jasontherion@gmail.com>
2019-06-23 22:35:13 -0500

    Correccion de errores

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,11 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  /**
+   * @params: meter parametro
+   * @return: Meter return
+   */
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
